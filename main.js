7f75471087d3cd9bc446422fa88b99197ddee6f3
Kamaree Robinson <kamareerobinson@gmail.com>
2022-11-01 11:09:10 -0400

    'first'

---
+++ main.js
@@ -0,0 +1,28 @@
+// Write a function named setAlarm which receives two parameters. The first parameter, employed, is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
+
+// The function should return true if you are employed and not on vacation (because these are the circumstances under which you need to set an alarm). It should return false otherwise. Examples:
+
+// setAlarm(true, true) -> false
+// setAlarm(false, true) -> false
+// setAlarm(false, false) -> false
+// setAlarm(true, false) -> true
+
+
+function setAlarm(employed, vacation){
+    if (employed === true && vacation === false){return true}
+      else{return false }
+    };
+
+
+
+// You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+// If it is a square, return its area. If it is a rectangle, return its perimeter.
+
+// Example(Input1, Input2 --> Output):
+
+// 6, 10 --> 32
+// 3, 3 --> 9
+// Note: for the purposes of this kata you will assume that it is a square if its length and width are equal, otherwise it is a rectangle.
+const areaOrPerimeter = function(l , w) {
+    if(l == w){return l*w}else{return (l*2)+(w*2)}
+    };    
